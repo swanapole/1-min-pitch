a8da02aa2f2daf6089927e1b679c98808e8cf303
swanapole <swanapoleshilla@yahoo.com>
2019-02-12 20:39:08 +0300

    deployment to heroku

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+
+class Config:
+
+
+    SECRET_KEY = '123@456'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:grown@localhost/pitches'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+class ProdConfig(Config):
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
